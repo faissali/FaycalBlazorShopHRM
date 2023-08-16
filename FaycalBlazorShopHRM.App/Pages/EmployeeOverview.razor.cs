7e28e71c9ca33d185e2e2a19fbcc1bbe030dfbf3
faycalbziou <faycalbziou@HOME>
2023-08-16 21:18:17 +0200

    add quick view

--- FaycalBlazorShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ FaycalBlazorShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -6,9 +6,15 @@ namespace FaycalBlazorShopHRM.App.Pages;
 public partial class EmployeeOverview
 {
     public List<Employee> Employees { get; set; } = default!;
+    private Employee? _selectedEmployee;
 
     protected override void OnInitialized()
     {
         Employees = MockDataService.Employees;
+    }
+
+    public void ShowQuickViewEmployee(Employee selectedEmployee)
+    {
+        _selectedEmployee = selectedEmployee;
     }
 }
