6689bb975afb274028868b618514293fbb173b54
faycalbziou <faycalbziou@HOME>
2023-08-16 20:39:51 +0200

    add employee card component

---
+++ FaycalBlazorShopHRM.App/Components/EmployeeCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace FaycalBlazorShopHRM.App.Components;
+
+public partial class EmployeeCard
+{
+    [Parameter]
+    public Employee Employee { get; set; } = default!;
+}
