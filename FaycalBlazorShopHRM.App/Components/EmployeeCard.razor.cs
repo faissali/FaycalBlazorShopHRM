e70254b53d5c92d520124f5825d03a2663197715
faycalbziou <faycalbziou@HOME>
2023-08-16 21:43:24 +0200

    add navigation

--- FaycalBlazorShopHRM.App/Components/EmployeeCard.razor.cs
+++ FaycalBlazorShopHRM.App/Components/EmployeeCard.razor.cs
@@ -10,4 +10,13 @@ public partial class EmployeeCard
 
     [Parameter]
     public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+    //[Inject]
+    //public NavigationManager NavigationManager { get; set; } = default!;
+
+    //public void NavigateToDetails(Employee employee)
+    //{
+    //    // code here ...
+    //    NavigationManager.NavigateTo($"/employeedetail/{employee.EmployeeId}");
+    //}
 }
---
+++ FaycalBlazorShopHRM.App/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,20 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using FaycalBlazorShopHRM.App.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FaycalBlazorShopHRM.App.Pages;
+
+public partial class EmployeeDetail
+{
+    [Parameter]
+    public string EmployeeId { get; set; } = string.Empty;
+
+    public Employee? Employee { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+        Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+
+        return base.OnInitializedAsync();
+    }
+}
