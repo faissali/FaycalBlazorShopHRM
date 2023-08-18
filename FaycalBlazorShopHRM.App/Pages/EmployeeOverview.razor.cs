6a15e837c9776ec5c673a63e0972b72fa5b61133
faycalbziou <faycalbziou@HOME>
2023-08-18 23:08:34 +0200

    communicate with the api

--- FaycalBlazorShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ FaycalBlazorShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -1,17 +1,23 @@
 ﻿using FaycalBlazorShopHRM.Shared.Domain;
 using FaycalBlazorShopHRM.App.Models;
+using Microsoft.AspNetCore.Components;
+using FaycalBlazorShopHRM.App.Services;
 
 namespace FaycalBlazorShopHRM.App.Pages;
 
 public partial class EmployeeOverview
 {
-    public List<Employee> Employees { get; set; } = default!;
     private Employee? _selectedEmployee;
 
+    [Inject]
+    private IEmployeeDataService? EmployeeDataService { get; set; }
+
+    public List<Employee>? Employees { get; set; }
+    
     private string Title = "Employee Overview";
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
-        Employees = MockDataService.Employees;
+        Employees = (await EmployeeDataService!.GetAllEmployees())?.ToList();
     }
 
     public void ShowQuickViewEmployee(Employee selectedEmployee)
--- FaycalBlazorShopHRM.App/Program.cs
+++ FaycalBlazorShopHRM.App/Program.cs
@@ -1,9 +1,15 @@
 using FaycalBlazorShopHRM.App;
+using FaycalBlazorShopHRM.App.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(
+    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+);
+
 
 await builder.Build().RunAsync();
