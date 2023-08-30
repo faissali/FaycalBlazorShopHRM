42f74a8f8e4410a1ef79310ca307d9ebce158660
faycalbziou <faycalbziou@HOME>
2023-08-30 23:23:55 +0200

    add form + validations

--- FaycalBlazorShopHRM.App/Pages/EmployeeEdit.razor.cs
+++ FaycalBlazorShopHRM.App/Pages/EmployeeEdit.razor.cs
@@ -1,6 +1,7 @@
 ﻿using FaycalBlazorShopHRM.App.Services;
 using FaycalBlazorShopHRM.Shared.Domain;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace FaycalBlazorShopHRM.App.Pages;
 
@@ -10,6 +11,10 @@ public partial class EmployeeEdit
     public List<Country>? Countries { get; set; } = new();
     public List<JobCategory>? JobCategories { get; set; } = new();
 
+    protected string Message = string.Empty;
+    protected string StatusClass = string.Empty;
+    protected bool Saved;
+    private IBrowserFile? selectedFile;
 
     [Inject]
     public IEmployeeDataService? EmployeeDataService { get; set; }
@@ -20,8 +25,70 @@ public partial class EmployeeEdit
     [Inject]
     public IJobCategoryDataService? JobCategoryDataService { get; set; }
 
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+
+
     [Parameter]
     public string? EmployeeId { get; set; }
+
+    protected async Task HandleValidSubmit()
+    {
+        Saved = false;
+
+        if (this.Employee!.EmployeeId != 0)
+        {
+            await EmployeeDataService?.UpdateEmployee(this.Employee)!;
+            this.Saved = true;
+            this.Message = "Employee updated";
+            this.StatusClass = "alert-success";
+        }
+        else
+        {
+            if (selectedFile != null)
+            {
+                var file = selectedFile;
+
+                Stream stream = file.OpenReadStream();
+                MemoryStream ms = new();
+                await stream.CopyToAsync(ms);
+                stream.Close();
+
+                Employee.ImageName = file.Name;
+                Employee.ImageContent = ms.ToArray();
+            }
+
+            var addedEmployee = await EmployeeDataService?.AddEmployee(this.Employee)!;
+
+            if (addedEmployee != null)
+            {
+                this.Saved = true;
+                this.Message = "New employee added";
+                this.StatusClass = "alert-success";
+            }
+            else
+            {
+                this.Saved = false;
+                this.Message = "Something went wrong adding the new employee. Please try again.";
+                this.StatusClass = "alert-danger";
+            }
+        }
+    }
+
+    protected void HandleInvalidSubmit()
+    {
+        this.Saved = false;
+        this.Message = "There are some validation errors. Please try again.";
+        this.StatusClass = "alert-danger";
+    }
+
+    protected async Task DeleteEmployee()
+    {
+        await this.EmployeeDataService?.DeleteEmployee(Employee!.EmployeeId)!;
+        this.Saved = true;
+        this.Message = "Employee deleted";
+        this.StatusClass = "alert-success";
+    }
 
     protected async override Task OnInitializedAsync()
     {
@@ -30,7 +97,7 @@ public partial class EmployeeEdit
 
         int.TryParse(this.EmployeeId, out var employeeId);
 
-        if(employeeId != 0)
+        if (employeeId != 0)
         {
             this.Employee = await this.EmployeeDataService?.GetEmployeeDetails(employeeId)!;
         }
@@ -45,5 +112,16 @@ public partial class EmployeeEdit
             };
         }
 
+    }
+
+    protected async Task OnInputFileChange(InputFileChangeEventArgs e)
+    {
+        selectedFile = e.File;
+        StateHasChanged();
+    }
+
+    public void NavigateToOverview()
+    {
+        NavigationManager.NavigateTo("/employeeoverview");
     }
 }
--- FaycalBlazorShopHRM.Shared/Domain/Employee.cs
+++ FaycalBlazorShopHRM.Shared/Domain/Employee.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FaycalBlazorShopHRM.Shared.Domain
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First Name is required")]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last Name is required")]
         public string LastName { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Email is required")]
         public string Email { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string Zip { get; set; } = string.Empty;
@@ -18,6 +29,8 @@ namespace FaycalBlazorShopHRM.Shared.Domain
         public bool Smoker { get; set; }
         public MaritalStatus MaritalStatus { get; set; }
         public Gender Gender { get; set; }
+
+        [StringLength(1000, ErrorMessage = "Comment is too long")]
         public string? Comment { get; set; }
         public DateTime? JoinedDate { get; set; }
         public DateTime? ExitDate { get; set; }
@@ -27,5 +40,10 @@ namespace FaycalBlazorShopHRM.Shared.Domain
 
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
+
+        public string? ImageName { get; set; }
+
+        [NotMapped]
+        public byte[]? ImageContent { get; set; }
     }
 }
